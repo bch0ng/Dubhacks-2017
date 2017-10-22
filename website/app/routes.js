33d79d4f5e0499799f6b7f7592e8477bbbf88c6f
bch0ng <bchong@uw.edu>
2017-10-22 07:57:11 -0700

    Update

--- website/app/routes.js
+++ website/app/routes.js
@@ -5,7 +5,13 @@ module.exports = function(app, passport) {
     // HOME PAGE (with login links) ========
     // =====================================
     app.get('/', function(req, res) {
-        res.render('index.ejs'); // load the index.ejs file
+        res.render('index.ejs', {
+            header: 'Protest!',
+            description: 'This is a protest!',
+            origin: 'ChIJHxilcfEUkFQRLnpHBhUg0zE',
+            destination: 'ChIJBW7jq_MUkFQRrB2d3BlrH-M',
+            twitterHandle: 'Dubhacks'
+        }); // load the index.ejs file
     });
 
     // =====================================
@@ -20,7 +26,7 @@ module.exports = function(app, passport) {
 
     // process the login form
     app.post('/login', passport.authenticate('local-login', {
-        successRedirect : '/profile', // redirect to the secure profile section
+        successRedirect : '/testing', // redirect to the secure profile section
         failureRedirect : '/login', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
@@ -51,6 +57,17 @@ module.exports = function(app, passport) {
     // we will use route middleware to verify this (the isLoggedIn function)
     app.get('/profile', isLoggedIn, function(req, res) {
         res.render('profile.ejs', {
+            user : req.user // get the user out of session and pass to template
+        });
+    });
+
+    // =====================================
+    // USER HOME =====================
+    // =====================================
+    // we will want this protected so you have to be logged in to visit
+    // we will use route middleware to verify this (the isLoggedIn function)
+    app.get('/userhome', isLoggedIn, function(req, res) {
+        res.render('userhome.ejs', {
             user : req.user // get the user out of session and pass to template
         });
     });
